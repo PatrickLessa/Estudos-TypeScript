0e14c2d8e2081417e2333be945cb2cf6c661025e
PATRTEI <patrick.teixeira@safra.com.br>
2021-01-29 15:52:13 -0300

    continuando estudos com tipos

--- tipos/tipos.js
+++ tipos/tipos.js
@@ -4,6 +4,7 @@ var nome = 'João';
 //nome = 27
 console.log(nome);
 // numbers
+// os numbers aceitam tanto valores inteiros quanto decimais
 var idade = 27;
 //idade = 'Idade'
 idade = 27.5;
@@ -55,3 +56,99 @@ var carro = "BMW";
 console.log(carro);
 carro = { marca: "BMW", ano: 2019 };
 console.log(carro);
+// funções
+// consigo definir o tipo de retorno (igual JAVA)
+function returnName() {
+    return nome;
+}
+console.log(returnName());
+// variavel que nao tem return
+function sayHi() {
+    console.log('Hi');
+}
+sayHi();
+// devemos passar a tipagem nos parametros
+function multiply(numA, numB) {
+    return numA * numB;
+}
+console.log(multiply(2, 4));
+// tipo função
+// quando falamos que uma variavel é do tipo "função"
+// fazemos uma sintaxe parecida com arrow function
+// onde definimos a qtd de parametro e o retorno. Exemplo: 
+var calculo; // após a flecha, passamos o tipo de retorno
+calculo = multiply;
+console.log(calculo(4, 7));
+// objetos
+var usuario = {
+    nome: 'Jucão',
+    idade: 27
+};
+console.log(usuario);
+usuario = {
+    nome: 'Maria',
+    idade: 26
+};
+console.log(usuario);
+// definindo o tipo da var
+var funcionario;
+var funcionario2;
+// atribuindo os valores
+funcionario = {
+    supers: ["Alvaro", "Marjoel"],
+    // podemos criar a function direto, mas podemos atribuir
+    // com funções criadas por fora
+    baterPonto: ponto
+};
+funcionario2 = {
+    supers: ["Puiggi", "Margerito"],
+    // podemos criar a function direto, mas podemos atribuir
+    // com funções criadas por fora
+    baterPonto: ponto
+};
+// criando a function
+function ponto(horario) {
+    if (horario <= 8) {
+        return 'Ponto normal';
+    }
+    else {
+        return 'Fora do horario';
+    }
+}
+console.log(funcionario.supers);
+console.log(funcionario.baterPonto(10));
+console.log(funcionario.baterPonto(8));
+// Union Types => onde podemos dizer que uma var pode receber tipos diferente
+// a sintaxe é usada com o pipe ("|"), como no exemplo
+var nota;
+nota = 10;
+console.log("Minha nota \u00E9 " + nota);
+nota = '10';
+console.log("Minha nota \u00E9 " + nota);
+// checando tipos
+var valor = 30;
+// checagem manual
+if (typeof valor === "number") {
+    console.log("é um number");
+}
+else {
+    console.log(typeof valor);
+}
+// retorno "never" => faz com que a function fique em loop (nunca saia da func)
+// ou termine com erro
+function falha(msg) {
+    throw new Error(msg);
+}
+var produto = {
+    nome: 'Sabão',
+    preco: 6,
+    validarProduto: function () {
+        if (!this.nome || this.nome.trim().length == 0) {
+            falha('precisa ter um nome');
+        }
+        if (this.preco <= 0) {
+            falha('preço inválido');
+        }
+    }
+};
+produto.validarProduto();
--- tipos/tipos.ts
+++ tipos/tipos.ts
@@ -4,6 +4,7 @@ let nome: string = 'João'
 console.log(nome)
 
 // numbers
+// os numbers aceitam tanto valores inteiros quanto decimais
 let idade: number = 27
 //idade = 'Idade'
 idade = 27.5
@@ -64,3 +65,132 @@ let carro: any = "BMW"
 console.log(carro)
 carro = { marca: "BMW", ano: 2019 }
 console.log(carro)
+
+// funções
+// consigo definir o tipo de retorno (igual JAVA)
+function returnName(): string{
+    return nome
+}
+
+console.log(returnName())
+
+// variavel que nao tem return
+function sayHi(): void{
+    console.log('Hi')
+}
+
+sayHi()
+
+// devemos passar a tipagem nos parametros
+function multiply(numA: number, numB: number): number{
+    return numA * numB
+}
+
+console.log(multiply(2, 4))
+
+// tipo função
+// quando falamos que uma variavel é do tipo "função"
+// fazemos uma sintaxe parecida com arrow function
+// onde definimos a qtd de parametro e o retorno. Exemplo: 
+let calculo: (numA: number, numB: number) => number // após a flecha, passamos o tipo de retorno
+
+calculo = multiply
+console.log(calculo(4, 7))
+
+// objetos
+let usuario: {nome: string, idade: number} = {
+    nome: 'Jucão',
+    idade: 27
+}
+
+console.log(usuario)
+
+usuario = {
+    nome: 'Maria',
+    idade: 26
+}
+
+console.log(usuario)
+
+// desafio funcionario
+
+// tipos personalizados (alias)
+// criando um tipo personalizado
+type Funcionario = {
+    supers: string[], 
+    baterPonto: (horas: number) => string
+}
+
+// definindo o tipo da var
+let funcionario: Funcionario
+
+let funcionario2: Funcionario
+
+// atribuindo os valores
+funcionario = {
+    supers: ["Alvaro", "Marjoel"],
+    // podemos criar a function direto, mas podemos atribuir
+    // com funções criadas por fora
+    baterPonto: ponto
+}
+
+funcionario2 = {
+    supers: ["Puiggi", "Margerito"],
+    // podemos criar a function direto, mas podemos atribuir
+    // com funções criadas por fora
+    baterPonto: ponto
+}
+
+// criando a function
+function ponto(horario: number): string{
+    if(horario <= 8){
+        return 'Ponto normal'
+    }else{
+        return 'Fora do horario'
+    }   
+}
+
+console.log(funcionario.supers)
+console.log(funcionario.baterPonto(10))
+console.log(funcionario.baterPonto(8))
+
+// Union Types => onde podemos dizer que uma var pode receber tipos diferente
+// a sintaxe é usada com o pipe ("|"), como no exemplo
+let nota: number | string
+nota = 10
+console.log(`Minha nota é ${nota}`)
+nota = '10'
+console.log(`Minha nota é ${nota}`)
+
+// checando tipos
+let valor = 30
+
+// checagem manual
+if(typeof valor === "number"){
+    console.log("é um number")
+}else{
+    console.log(typeof valor)
+}
+
+// retorno "never" => faz com que a function fique em loop (nunca saia da func)
+// ou termine com erro
+
+function falha(msg: string): never{
+    throw new Error(msg)
+}
+
+const produto = {
+    nome: 'Sabão',
+    preco: 6,
+    validarProduto(){
+        if(!this.nome || this.nome.trim().length == 0){
+            falha('precisa ter um nome')
+        }
+
+        if(this.preco <= 0){
+            falha('preço inválido')
+        }
+    }
+}
+
+produto.validarProduto()
