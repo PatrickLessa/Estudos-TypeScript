b8cdd44e1c5914619f8dae0ff229cbe30aa21e29
PATRTEI <patrick.teixeira@safra.com.br>
2021-02-01 11:16:48 -0300

    capitulo do compilador TS completo

--- compilador/compilador.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-let canal = 'Gaveta';
-let inscritos = 610245;
-// canal = inscritos
-console.log(`canal = ${canal}`);
-// let nome = 'Pedro'
-//# sourceMappingURL=compilador.js.map
--- compilador/compilador.ts
+++ compilador/compilador.ts
@@ -5,3 +5,22 @@ let inscritos: number = 610245
 console.log(`canal = ${canal}`)
 
 // let nome = 'Pedro'
+
+function soma(a: any, b: any){
+    return a + b
+}
+
+let qlqrCoisa
+qlqrCoisa = 12
+qlqrCoisa = 'abc'
+
+function saudar(isManha: boolean, /* horas: number -> exemplo de noUnusedParameters*/ ): string{
+    // let a = 1 -> exemplo de noUnusedLocals
+    let saudacao: string
+    if(isManha){
+        saudacao = 'Bom dia'
+    }else{
+        saudacao = "boa vida"
+    }
+    return saudacao
+}
